ed53537011e468b056725d369efe02f4fb646e59
张浩 <15752224+zhang-hao-073@user.noreply.gitee.com>
2025-10-23 15:27:35 +0800

    linkedList

--- src/leetcode100/Test.java
+++ src/leetcode100/Test.java
@@ -1,23 +1,17 @@
 package leetcode100;
 
+import java.util.HashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Test {
     public static void main(String[] args) {
-        Function<String,Integer> function = s -> {
-            int sum = 0;
-            for(Character c : s.toCharArray()){
-                sum += c - 'a' + 1;
-            }
-            return sum;
-        };
-        String str = "ashcj";
-        String str2 = "ashc";
-        test(str,str1 -> str1.length() );
-        // 函数式接口，上面的函数表示str,为传递的参数，str1 -> str1.length()表示函数体，返回值为str1的长度
-        Consumer<String> consumer = System.out::println;
-        test2("1234",s -> System.out.println(s.length()));
+        Map<Integer,String> map = new HashMap<>();
+        Set<Integer> set = new LinkedHashSet<>();
+
     }
 
     public static void test(String str,Function<String,Integer> function){

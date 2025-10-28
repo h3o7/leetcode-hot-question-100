556f5c6870ab0d714ab5ad4f73c656d20a9f8c18
张浩 <15752224+zhang-hao-073@user.noreply.gitee.com>
2025-10-28 18:13:51 +0800

    linkedList

---
+++ src/leetcode100/binary/tree/Question3.java
@@ -0,0 +1,16 @@
+package leetcode100.binary.tree;
+
+public class Question3 {
+
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+
+    }
+
+}
---
+++ src/leetcode100/binary/tree/Question5.java
@@ -0,0 +1,18 @@
+package leetcode100.binary.tree;
+
+public class Question5 {
+
+    int ans = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return ans - 1;
+    }
+    public int depth(TreeNode root) {
+        if (root == null) return 0;
+        int left = depth(root.left);
+        int right = depth(root.right);
+        ans = Math.max(ans, left + right + 1);
+        return Math.max(left, right) + 1;
+    }
+
+}

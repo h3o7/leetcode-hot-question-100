6116f64a9f52b039df53bf2681a3abb54909398b
张浩 <15752224+zhang-hao-073@user.noreply.gitee.com>
2025-12-06 17:37:42 +0800

    maxProfit

---
+++ src/leetcode100/greed/Question1.java
@@ -0,0 +1,26 @@
+package leetcode100.greed;
+
+public class Question1 {
+    public static void main(String[] args) {
+        int[] prices = {2, 1, 2, 1, 0, 1, 2};
+        System.out.println(maxProfit(prices));
+    }
+
+
+    public static int maxProfit(int[] prices) {
+        if (prices.length < 2) return 0;
+        int profit = 0;
+        int buy = Integer.MAX_VALUE;
+        for (int num : prices) {
+            if (num < buy) {
+                buy = num;
+            } else {
+                int temp = num - buy;
+                profit = temp > profit ? temp : profit;
+            }
+        }
+        return profit;
+
+    }
+
+}
